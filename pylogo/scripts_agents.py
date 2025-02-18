036184efa7d830b73b0b5a9ce779ff2a78592f5b
Suman Saurabh <sumanan047@gmail.com>
2025-02-18 06:17:06 -0600

    Add support for saving simulation and creating animation

--- pylogo/scripts_agents.py
+++ pylogo/scripts_agents.py
@@ -1,7 +1,10 @@
-from pylogo.pylogo.agent import Agent, AgentSet
-from pylogo.pylogo.distributions import Distribution_2D, Distribution_1D
+from pylogo.agent import Agent, AgentSet
+from pylogo.simulation import simulation, Time
+from pylogo.rules import move_by_at_angle, move_up
+from pylogo.distributions import Distribution_2D, Distribution_1D
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import numpy as np
 
 # create distributions
 d1 = Distribution_2D()
@@ -11,16 +14,44 @@ d2.uniform(low=[0.5,0.5], high=[0.5,0.5], size=100)
 d3 = Distribution_1D()
 d3.normal(mean=33, std=10, size=100)
 
+# make an agent
+ag1 = Agent()
 # unwrap d1.x_arr and d1.y_arr to fill AgentSet positions
-ag1 = AgentSet(number = 100, age=d3)
-ag_list = ag1.make_agents(position_dist=d1, size_dist=d2, color=(1,0,0))
+# ag1 = AgentSet(number = 100, position_dist=d1, size_dist=d2, age=d3)
+# ag_list = ag1.make_agents(position_dist=d1, size_dist=d2, color=(1,0,0))
 
-# visualize the agents
+# make a time object
+# _t = Time(0, 10, 0.1)
+# # make a simulation
+# sim = simulation({ag1: [move_by_at_angle]}, _t)
+# sim.run_simulation(distance=1, angle=0.1)
+# sim.save_simulation()
+
+
+
+
+# plot the simulation
+# read the data from the file simulation.csv
+data = pd.read_csv('simulation.csv')
+x = []
+y = []
+import matplotlib.animation as animation
+
 fig, ax = plt.subplots()
-ax.set_xlim(-15, 15)
-ax.set_ylim(-15, 15)
-for age in ag_list:
-    ax.add_patch(age.sprite)
-plt.show()
-plt.hist(ag1.agentset_properties['age'].data)
+x_lim = 1000
+y_lim = 1000
+ax.set_xlim(-x_lim,y_lim)
+ax.set_ylim(-x_lim,y_lim)
+ax.set_aspect('equal')
+line, = ax.plot([], [], 'r-')
+
+def animate(i):
+    move_by_at_angle(ag1, distance=i**0.1, angle=np.pi/3*i**2)
+    x.append(ag1.x_pos)
+    y.append(ag1.y_pos)
+    line.set_data(x, y)
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, frames=4, interval=1)
+ani.save('animation.gif', writer='pillow')
 plt.show()
