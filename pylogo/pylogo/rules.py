724a65b471fe9e75f6d962d85917040feab84820
Suman Saurabh <sumanan047@gmail.com>
2025-02-20 23:35:31 -0600

    Refactor move functions to accept AgentSet

--- pylogo/pylogo/rules.py
+++ pylogo/pylogo/rules.py
@@ -6,40 +6,75 @@ import numpy as np
 from typing import Union
 
 # a collection of simple rules for agents
-def move_to(bound_agent, x, y):
-    bound_agent.x_pos = x
-    bound_agent.y_pos = y
-    # update the dicts
-    bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
-    bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
+def move_to(bound_agent: Union[Agent, AgentSet], x, y):
+    if not isinstance(bound_agent, (Agent, AgentSet)):
+        raise ValueError("The bound_agent must be an instance of the Agent or AgentSet class.")
+    elif isinstance(bound_agent, AgentSet):
+        for ag in bound_agent.agents:
+            move_to(ag, x, y)
+    else:
+        bound_agent.x_pos = x
+        bound_agent.y_pos = y
+        # update the dicts
+        bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
+        bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
 
-def move_by(bound_agent, dx = 1, dy = 1):
-    bound_agent.x_pos += dx
-    bound_agent.y_pos += dy
-    # update the dicts
-    bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
-    bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
+def move_by(bound_agent: Union[Agent, AgentSet], dx = 1, dy = 1):
+    if not isinstance(bound_agent, (Agent, AgentSet)):
+        raise ValueError("The bound_agent must be an instance of the Agent or AgentSet class.")
+    elif isinstance(bound_agent, AgentSet):
+        for ag in bound_agent.agents:
+            move_by(ag, dx, dy)
+    else:
+        bound_agent.x_pos += dx
+        bound_agent.y_pos += dy
+        # update the dicts
+        bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
+        bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
 
-def move_up(bound_agent, distance=1):
-    # print("Moving up")
-    bound_agent.y_pos += distance
-    # update the dicts
-    bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
+def move_up(bound_agent: Union[Agent, AgentSet], distance=1):
+    if not isinstance(bound_agent, (Agent, AgentSet)):
+        raise ValueError("The bound_agent must be an instance of the Agent or AgentSet class.")
+    elif isinstance(bound_agent, AgentSet):
+        for ag in bound_agent.agents:
+            move_up(ag, distance)
+    else:
+        bound_agent.y_pos += distance
+        # update the dicts
+        bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
 
-def move_down(bound_agent, distance=1):
-    bound_agent.y_pos -= distance
-    # update the dicts
-    bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
+def move_down(bound_agent: Union[Agent, AgentSet], distance=1):
+    if not isinstance(bound_agent, (Agent, AgentSet)):
+        raise ValueError("The bound_agent must be an instance of the Agent or AgentSet class.")
+    elif isinstance(bound_agent, AgentSet):
+        for ag in bound_agent.agents:
+            move_down(ag, distance)
+    else:
+        bound_agent.y_pos -= distance
+        # update the dicts
+        bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
 
-def move_left(bound_agent, distance=1):
-    bound_agent.x_pos -= distance
-    # update the dicts
-    bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
+def move_left(bound_agent: Union[Agent, AgentSet], distance=1):
+    if not isinstance(bound_agent, (Agent, AgentSet)):
+        raise ValueError("The bound_agent must be an instance of the Agent or AgentSet class.")
+    elif isinstance(bound_agent, AgentSet):
+        for ag in bound_agent.agents:
+            move_left(ag, distance)
+    else:
+        bound_agent.x_pos -= distance
+        # update the dicts
+        bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
 
-def move_right(bound_agent, distance=1):
-    bound_agent.x_pos += distance
-    # update the dicts
-    bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
+def move_right(bound_agent: Union[Agent, AgentSet], distance=1):
+    if not isinstance(bound_agent, (Agent, AgentSet)):
+        raise ValueError("The bound_agent must be an instance of the Agent or AgentSet class.")
+    elif isinstance(bound_agent, AgentSet):
+        for ag in bound_agent.agents:
+            move_right(ag, distance)
+    else:
+        bound_agent.x_pos += distance
+        # update the dicts
+        bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
 
 def move_by_at_angle(bound_agent: Union[Agent, AgentSet], distance, angle):
     if not isinstance(bound_agent, (Agent, AgentSet)):
@@ -54,5 +89,3 @@ def move_by_at_angle(bound_agent: Union[Agent, AgentSet], distance, angle):
         # update the dicts
         bound_agent.agent_dict['x_pos'] = bound_agent.x_pos
         bound_agent.agent_dict['y_pos'] = bound_agent.y_pos
-
-# a collection of simple rules for agentsets
--- pylogo/tests/test_rules.py
+++ pylogo/tests/test_rules.py
@@ -1,11 +1,22 @@
 from pylogo.rules import move_to, move_by, move_up, move_down, move_left, move_right, move_by_at_angle
-from pylogo.agent import Agent
+from pylogo.distributions import Distribution_1D, Distribution_2D
+from pylogo.agent import Agent, AgentSet
 import pytest
 import numpy as np
 
 @pytest.fixture
 def _agent():
     return Agent()
+
+@pytest.fixture
+def _agent_set():
+    NO_AGENTS = 5
+    d1 = Distribution_2D()
+    d1.uniform(low=[-20,-20], high=[50,70], size=NO_AGENTS)
+
+    d2 = Distribution_2D()
+    d2.uniform(low=[0.5,0.5], high=[0.5,0.5], size=NO_AGENTS)
+    return AgentSet(number=5,position_dist=d1,size_dist=d2)
 
 def test_move_to(_agent):
     _agent.register_rule('move_to', move_to)
@@ -49,3 +60,44 @@ def test_move_by_at_angle(_agent):
     assert np.isclose(_agent.x_pos, 0.0, atol=1e-2)  # approximate value with tolerance of 0.01
     assert np.isclose(_agent.y_pos, 1.0, atol=1e-2)  # approximate value with tolerance of 0.01
 
+
+#now testing for AgentSet
+def test_move_to_agentset(_agent_set):
+    agent_set = _agent_set
+    for agent in agent_set:
+        agent.register_rule('move_to', move_to)
+    move_to(agent_set, 5, 10)
+    assert agent_set[0].x_pos == 5
+    assert agent_set[0].y_pos == 10
+
+def test_move_up_agentset(_agent_set):
+    agent_set = _agent_set
+    for agent in agent_set:
+        agent.register_rule('move_up', move_up)
+    agent_set[0].y_pos = 5
+    move_up(agent_set, 2)
+    assert agent_set[0].y_pos == 7
+
+def test_move_down_agentset(_agent_set):
+    agent_set = _agent_set
+    for agent in agent_set:
+        agent.register_rule('move_down', move_down)
+    agent_set[0].y_pos = 5
+    move_down(agent_set, 2)
+    assert agent_set[0].y_pos == 3
+
+def test_move_left_agentset(_agent_set):
+    agent_set = _agent_set
+    for agent in agent_set:
+        agent.register_rule('move_left', move_left)
+    agent_set[0].x_pos = 5
+    move_left(agent_set, 2)
+    assert agent_set[0].x_pos == 3
+
+def test_move_right_agentset(_agent_set):
+    agent_set = _agent_set
+    for agent in agent_set:
+        agent.register_rule('move_right', move_right)
+    agent_set[0].x_pos = 5
+    move_right(agent_set, 2)
+    assert agent_set[0].x_pos == 7
