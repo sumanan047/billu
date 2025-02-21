90c61fc196849d342ff283759bf629e67b6b9e21
Suman Saurabh <sumanan047@gmail.com>
2025-02-20 22:28:42 -0600

    Add Time class and Simulation class to pylogo/pylogo/simulation.py

--- pylogo/pylogo/simulation.py
+++ pylogo/pylogo/simulation.py
@@ -12,6 +12,83 @@ class Time:
         self.nos_time_step = nos_time_step
         self.current_time = start_time # start with the start_time and then change
 
+    def __str__(self):
+        return f"Time object with start_time: {self.start_time}, end_time: {self.end_time}, nos_time_step: {self.nos_time_step}"
+    
+    def time_array(self):
+        return np.linspace(self.start_time, self.end_time, self.nos_time_step)
+
+    def __iter__(self):
+        self.current_time = self.start_time
+        return self
+
+    def __next__(self):
+        if self.current_time < self.end_time:
+            self.current_time += (self.end_time - self.start_time) / self.nos_time_step
+            return self.current_time
+        else:
+            raise StopIteration
+
+
+class Simulation:
+    def __init__(self, sim_agent_rules: dict, _time: Time = None):
+        if len(list(sim_agent_rules.keys())) == 0:
+            raise ValueError("The simulation agent rules dictionary cannot be empty.")
+        self.sim_agent_rules = sim_agent_rules
+        self._time = _time
+        self.data = None # dataframe to store the simulation data
+        for key, value in sim_agent_rules.items():
+            if isinstance(key, (Agent, AgentSet)) and all(callable(func) for func in value):
+                # if the key is an instance of AgentSet, then register the rules for all the agents in the AgentSet
+                if isinstance(key, AgentSet):
+                    for ag in key.agents:
+                        for v in value:
+                            ag.register_rule(str(v.__name__), v)
+                # if the key is an instance of Agent, then simply register the rules for the Agent
+                else:
+                    for v in value:
+                        key.register_rule(str(v.__name__), v)
+            else:
+                raise ValueError("rules_list must contain only callable objects.")
+
+    def run_simulation(self,
+                       export = True,
+                       filename = "simulation", *args, **kwargs):
+        """
+        User should ovveride this method to run the simulation.
+        """
+        # bind all the values in the self.sim_agent_rules to the key in the dictionary
+        # and then call the function with the arguments and keyword arguments
+        for key, value in self.sim_agent_rules.items():
+            if isinstance(key, AgentSet):
+                for ag in key.agents:
+                    for v in value:
+                        # iterate through all the functions in the value list
+                        # Get the parameters of the function
+                        params = v.__code__.co_varnames[:v.__code__.co_argcount]
+                        # Filter the arguments and keyword arguments based on the function parameters
+                        filtered_args = [arg for arg in args if arg in params]
+                        filtered_kwargs = {k: v for k, v in kwargs.items() if k in params}
+                        # Call the function with the filtered arguments and keyword arguments
+                        ag.__dict__[v.__name__](*filtered_args, **filtered_kwargs)
+            else:
+                # keys are the agents and values are the functions to be called
+                for v in value:
+                    # iterate through all the functions in the value list
+                    # Get the parameters of the function
+                    params = v.__code__.co_varnames[:v.__code__.co_argcount]
+                    # Filter the arguments and keyword arguments based on the function parameters
+                    filtered_args = [arg for arg in args if arg in params]
+                    filtered_kwargs = {k: v for k, v in kwargs.items() if k in params}
+                    # Call the function with the filtered arguments and keyword arguments
+                    key.__dict__[v.__name__](*filtered_args, **filtered_kwargs)
+
+    def save_simulation(self):
+        """
+        User should ovveride this method to save the simulation.
+        """
+        print("Simulation saved.")
+
     def __iter__(self):
         self.current_time = self.start_time
         return self
--- pylogo/tests/test_simulation.py
+++ pylogo/tests/test_simulation.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from pylogo.simulation import Time, simulation
+from pylogo.simulation import Time, Simulation
 from pylogo.agent import Agent
 from pylogo.rules import move_by_at_angle, move_up, move_down, move_left, move_right
 
@@ -14,32 +14,32 @@ def agent2():
     return Agent()
 
 def test_time_array():
-    t = Time(0, 10, 0.1)
+    t = Time(0, 10, 100)
     time_array = t.time_array()
     assert len(time_array) == 100
     assert time_array[0] == 0
-    assert time_array[-1] == 9.9
+    assert time_array[-1] == 10.0
 
 def test_time_iter():
-    t = Time(0, 10, 0.1)
+    t = Time(0, 10, 10)
     for time in t:
         assert time >= 0
         assert time <= 10
 
 def test_time_next():
-    t = Time(0, 10, 0.1)
+    t = Time(0, 10, 100)
     time = next(t)
     assert time == 0.1
     time = next(t)
     assert time == 0.2
 
 def test_time_str():
-    t = Time(0, 10, 0.1)
-    assert str(t) == "Time object with start_time: 0, end_time: 10, time_step: 0.1"
+    t = Time(0, 10, 1)
+    assert str(t) == "Time object with start_time: 0, end_time: 10, nos_time_step: 1"
 
 def test_simulation_init():
     with pytest.raises(ValueError):
-        sim = simulation({}, Time(0, 10, 0.1))
+        sim = Simulation({}, Time(0, 10, 0.1))
 
 
 def test_simulation_run_simulation(agent1, agent2):
@@ -47,6 +47,6 @@ def test_simulation_run_simulation(agent1, agent2):
         agent1: [move_by_at_angle, move_up],
         agent2: [move_down, move_left]
     }
-    sim = simulation(sim_agent_rules, Time(0, 10, 0.1))
+    sim = Simulation(sim_agent_rules, Time(0, 10, 0.1))
     sim.run_simulation(distance=1, angle=np.pi/10)
-    assert sim.data is not None
+    assert agent1.agent_dict is not None
