0aa6290d36a571144e4bd0b1493b4c7a601f2a2b
Suman Saurabh <sumanan047@gmail.com>
2025-03-06 21:25:31 -0600

    Increase number of agents and add energy property

--- pylogo/script_simulation.py
+++ pylogo/script_simulation.py
@@ -4,20 +4,24 @@ import numpy as np
 from pylogo.agent import Agent, AgentSet
 from pylogo.distributions import Distribution_1D, Distribution_2D
 from pylogo.simulation import Simulation, Time
-from pylogo.rules import move_by_at_angle, move_up, move_randomly
+from pylogo.rules import move_by_at_angle, move_up, move_randomly, decrement_property_agent
 
-NO_AGENTS = 5
+NO_AGENTS = 10
 
 # make distributions
 d1 = Distribution_2D()
 d1.uniform(low=[-20,-20], high=[50,70], size=NO_AGENTS)
-
 d2 = Distribution_2D()
 d2.uniform(low=[0.5,0.5], high=[0.5,0.5], size=NO_AGENTS)
+
 # make an agent
 ag = Agent()
 agset = AgentSet(number = NO_AGENTS, position_dist=d1, size_dist=d2, color=(1, 0, 0))
+agset.set_properties(energy=100)
+
+# Make time # that is a wonderful thing to say
 _t = Time(0, 10, 1)
+
 
 fig, ax = plt.subplots(figsize=(10, 10))
 x_lim = 100
@@ -33,11 +37,14 @@ def update(frame):
     ax.set_xlim(-x_lim, y_lim)
     ax.set_ylim(-x_lim, y_lim)
     ax.set_aspect('equal')
-    sim = Simulation({agset: [move_randomly]}, _t)
-    sim.run_simulation(distance_range=[0,10], angle=[0, 2*np.pi])
+    sim = Simulation({agset: [move_randomly, decrement_property_agent]}, _t)
+    sim.run_simulation(distance_range=[0,10], angle=[0, 2*np.pi], prop_name = "energy", decrement=1)
     ax.plot([ag.x_pos for ag in agset.agents], [ag.y_pos for ag in agset.agents], 'rx')
+    # for ag in agset.agents:
+    #     ax.annotate(f"{ag.agent_dict['energy']}", (ag.x_pos, ag.y_pos))
+    sim.save_simulation()
 
-ani = animation.FuncAnimation(fig, update, frames=100, interval=100, repeat=False)
+ani = animation.FuncAnimation(fig, update, frames=1, interval=1, repeat=False)
 
 ani.save('animation.gif', writer='pillow')
 plt.show()
